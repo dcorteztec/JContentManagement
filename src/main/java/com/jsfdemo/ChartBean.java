3a6081efb024f53201dfba217f3dee81a058f877
David de Amorim Cortez <dcorteztec@gmail.com>
2013-04-19 13:52:18 -0300

    commit performed for application running on tomcat 7

--- src/main/java/com/jsfdemo/ChartBean.java
+++ src/main/java/com/jsfdemo/ChartBean.java
@@ -3,16 +3,23 @@ package com.jsfdemo;
 import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
 
 import org.primefaces.event.ItemSelectEvent;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.PieChartModel;
 
+@ManagedBean
+@SessionScoped
 public class ChartBean implements Serializable{
     
-    private CartesianChartModel categoryModel;  
+	private static final long serialVersionUID = 1L;
+
+	private CartesianChartModel categoryModel;  
     
     private PieChartModel pieModel;  
   
@@ -42,20 +49,20 @@ public class ChartBean implements Serializable{
         ChartSeries boys = new ChartSeries();  
         boys.setLabel("Boys");  
   
-        //boys.set("2004", 120);  
-        //boys.set("2005", 100);  
-        //boys.set("2006", 44);  
-        //boys.set("2007", 150);  
-        //boys.set("2008", 25);  
+        boys.set("2004", 120);  
+        boys.set("2005", 100);  
+        boys.set("2006", 44);  
+        boys.set("2007", 150);  
+        boys.set("2008", 25);  
   
         ChartSeries girls = new ChartSeries();  
         girls.setLabel("Girls");  
   
-        //girls.set("2004", 52);  
-        //girls.set("2005", 60);  
-        //girls.set("2006", 110);  
-        //girls.set("2007", 135);  
-        //girls.set("2008", 120);  
+        girls.set("2004", 52);  
+        girls.set("2005", 60);  
+        girls.set("2006", 110);  
+        girls.set("2007", 135);  
+        girls.set("2008", 120);  
   
         categoryModel.addSeries(boys);  
         categoryModel.addSeries(girls);  
@@ -64,10 +71,10 @@ public class ChartBean implements Serializable{
     private void createPieModel() {  
         pieModel = new PieChartModel();  
   
-        //pieModel.set("Brand 1", 540);  
-        //pieModel.set("Brand 2", 325);  
-        //pieModel.set("Brand 3", 702);  
-        //pieModel.set("Brand 4", 421);  
+        pieModel.set("Brand 1", 540);  
+        pieModel.set("Brand 2", 325);  
+        pieModel.set("Brand 3", 702);  
+        pieModel.set("Brand 4", 421);  
     } 
 
 }
